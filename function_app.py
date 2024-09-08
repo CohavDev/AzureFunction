cbc5fc6686077f825b9cbc0b0ca54f4c06fffea5
CohavDev <shaulspam@gmail.com>
2024-09-08 16:28:28 +0300

    first attempt to substring events

--- function_app.py
+++ function_app.py
@@ -1,4 +1,6 @@
 import azure.functions as func
+import re
+import json
 import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -6,20 +8,70 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        events_str_lst = req_body.get('events')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    if events_str_lst:
+        events_json = json.dumps(events_str_lst)
+        response_object =  process_events(events_json)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(response_object, mimetype="application/json", status_code = 200)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             "This HTTP triggered function executed without list of events. Pass events in the request body.",
+             status_code=400
         )
+def process_events(events:list)->str:
+    events_info_lst = []
+    events_wrong_format_lst = []
+    for event in events:
+        event_body = event["body"]
+        event_info = extract_info_from_body(event_body)
+        if event_info == None:
+            events_wrong_format_lst.append(event_info)
+        else:
+            events_info_lst.append(event_info)
+    response_object = {
+        "ok_list":events_info_lst,
+        "bad_list":events_wrong_format_lst
+    }
+    return json.dumps(response_object)
+
+def extract_info_from_body(input:str):
+    match_words = {
+        "tech_name":"שם טכנאי",
+        "mantis_number":"מנטיס",
+        "client_name":"שם לקוח",
+        "service_type":"סוג השירות",
+        "vhcls_count":"כמות רכבים",
+        "car_item":"רכב מספר"
+    }
+    tech_name = find_word_after(input, match_words["tech_name"])
+    mantis = find_word_after(input, match_words["mantis_number"])
+    client = find_word_after(input, match_words["client_name"])
+    service = find_word_after(input, match_words["service_type"])
+    cars_count = find_word_after(input, match_words["vhcls_count"])
+    if any(x is None for x in (tech_name, mantis, client, service, cars_count)):
+        # wrong format
+        return None
+    else:
+        data = {"tech_name":tech_name, "mantis": mantis, "client": client, "service": service}
+        for i in range(int(cars_count)):
+            car_id_name = match_words["car_item"]+" " + i + 1
+            car_license = find_word_after(input, car_id_name)
+            if car_license == None:
+                return None
+            else:
+                data[car_id_name] = car_license
+        return json.dumps(data)
+
+
+def find_word_after(input:str, match_word:str):
+    pattern = rf'\b{re.escape(match_word)}\s+(\w+)'
+    match = re.search(pattern, input)
+    return match.group(1) if match else None
+
