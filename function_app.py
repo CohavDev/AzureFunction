a226f2cbbed699bdf6c69036f5672d326733cf90
CohavDev <shaulspam@gmail.com>
2024-09-09 09:39:47 +0300

    function is alive

--- function_app.py
+++ function_app.py
@@ -5,50 +5,60 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
     try:
         req_body = req.get_json()
     except ValueError:
-        pass
+        return func.HttpResponse(
+            "Error: Request body is not valid JSON", status_code=400
+        )
     else:
-        events_str_lst = req_body.get('events')
+        events_str_lst = req_body.get("events")
 
     if events_str_lst:
-        events_json = json.dumps(events_str_lst)
-        response_object =  process_events(events_json)
+        # events_json = json.loads(events_str_lst)
+        try:
+            response_object = process_events(events_str_lst)
+        except Exception as e:
+            return func.HttpResponse("Events Processing Failed", status_code=500)
 
-        return func.HttpResponse(response_object, mimetype="application/json", status_code = 200)
+        return func.HttpResponse(
+            response_object, mimetype="application/json", status_code=200
+        )
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed without list of events. Pass events in the request body.",
-             status_code=400
+            "This HTTP triggered function executed without list of events. Pass events in the request body.",
+            status_code=400,
         )
-def process_events(events:list)->str:
+
+
+def process_events(events: list) -> str:
     events_info_lst = []
     events_wrong_format_lst = []
     for event in events:
+        print("event")
+        print(event)
         event_body = event["body"]
         event_info = extract_info_from_body(event_body)
         if event_info == None:
-            events_wrong_format_lst.append(event_info)
+            events_wrong_format_lst.append(event["subject"])
         else:
             events_info_lst.append(event_info)
-    response_object = {
-        "ok_list":events_info_lst,
-        "bad_list":events_wrong_format_lst
-    }
+    response_object = {"ok_list": events_info_lst, "bad_list": events_wrong_format_lst}
     return json.dumps(response_object)
 
-def extract_info_from_body(input:str):
+
+def extract_info_from_body(input: str):
     match_words = {
-        "tech_name":"שם טכנאי",
-        "mantis_number":"מנטיס",
-        "client_name":"שם לקוח",
-        "service_type":"סוג השירות",
-        "vhcls_count":"כמות רכבים",
-        "car_item":"רכב מספר"
+        "tech_name": "שם טכנאי",
+        "mantis_number": "מנטיס",
+        "client_name": "שם לקוח",
+        "service_type": "סוג השירות",
+        "vhcls_count": "כמות רכבים",
+        "car_item": "רכב מספר",
     }
     tech_name = find_word_after(input, match_words["tech_name"])
     mantis = find_word_after(input, match_words["mantis_number"])
@@ -59,9 +69,14 @@ def extract_info_from_body(input:str):
         # wrong format
         return None
     else:
-        data = {"tech_name":tech_name, "mantis": mantis, "client": client, "service": service}
+        data = {
+            "tech_name": tech_name,
+            "mantis": mantis,
+            "client": client,
+            "service": service,
+        }
         for i in range(int(cars_count)):
-            car_id_name = match_words["car_item"]+" " + i + 1
+            car_id_name = match_words["car_item"] + " " + i + 1
             car_license = find_word_after(input, car_id_name)
             if car_license == None:
                 return None
@@ -70,8 +85,7 @@ def extract_info_from_body(input:str):
         return json.dumps(data)
 
 
-def find_word_after(input:str, match_word:str):
-    pattern = rf'\b{re.escape(match_word)}\s+(\w+)'
+def find_word_after(input: str, match_word: str):
+    pattern = rf"\b{re.escape(match_word)}\s+(\w+)"
     match = re.search(pattern, input)
     return match.group(1) if match else None
-
